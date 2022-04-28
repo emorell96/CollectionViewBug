7337dc80903c4db2e0f74dfb5de686f5770180d7
Henry <emorell96@hotmail.com>
2022-04-28 22:32:59 +0200

    Add project files.

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		BindingContext = this;
+		InitializeComponent();
+	}
+	public ObservableCollection<TestItem> Items { get; } = new ObservableCollection<TestItem>();
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		Items.Clear();
+		for(int i = 0; i < 10; i++)
+        {
+			//remove this and you will see it bug out too.
+			await Task.Delay(10);
+			Items.Add(new TestItem
+			{
+				Name = $"Test Item {i}",
+				Description = "Description"
+			});
+		}
+		
+	}
+}
+
+public class TestItem
+{
+	public string Name { get; set; }
+	public string Description { get; set; }
+}
+
